a88eda96b9822b0dfb703f9ba173125e19ceed7c
Tiffany <n0isehazard@gmail.com>
2018-08-15 17:49:50 -0700

    Style login page

--- resources/assets/js/components/LoginForm.vue
+++ resources/assets/js/components/LoginForm.vue
@@ -1,33 +1,31 @@
 <template>
     <form method="POST" :action="loginUrl()" accept-charset="UTF-8">
         <input name="_token" type="hidden" :value="csrfToken()">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form__group">
-                    <label for="username" class="label--required">Username or Email</label>
-                    <input name="username" type="text" id="username">
+            <form>
+                <div class="form-group">
+                    <label for="username" class="form-control--required">Username or Email</label>
+                    <input class="form-control" name="username" type="text" id="username">
                 </div>
-                <div class="form__group">
-                    <label for="password" class="label--required">Password</label>
-                    <input name="password" type="password" id="password">
+                <div class="form-group">
+                    <label for="password" class="form-control--required">Password</label>
+                    <input class="form-control" name="password" type="password" id="password">
                 </div>
-                <div class="form__group">
+                <div class="form-group">
                     <loading-button v-bind:title="'Log-In'"></loading-button>
                 </div>
-            </div>
-        </div>
+            </form>
     </form>
 </template>
 
 <script>
-    export default {
-        methods: {
-            csrfToken: function () {
-                return window.appToken;
-            },
-            loginUrl: function () {
-                return window.appURL + '/login';
-            }
-        }
+export default {
+  methods: {
+    csrfToken: function() {
+      return window.appToken;
+    },
+    loginUrl: function() {
+      return window.appURL + "/login";
     }
+  }
+};
 </script>
