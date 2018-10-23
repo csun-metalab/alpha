7be161430394b6ca772a0b387c6927280ee622e8
luisjg <luis.guzman@metalab.csun.edu>
2018-10-23 11:50:01 -0700

    Update the avatar image retrieval.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -20,19 +20,11 @@ class LoginController extends Controller
             $guzzle = HandlerGuzzleFactory::fromDefaults();
             $directoryResponse = $guzzle->get(config('webservices.directory_url').
                 'members/email/'. auth()->user()->email);
-            $mediaResponse = $guzzle->get(config('webservices.media_url').'faculty/media/'.auth()->user()->email_uri);
-            $mediaResponse = $guzzle->resolveResponseBody($mediaResponse, 'json');
-            $avatar_url = '';
-            $guzzle->setRequestOptionArray([
-                'on_stats' => function (TransferStats $stats) use (&$avatar_url) {
-                    $avatar_url = $stats->getEffectiveUri();
-                    $avatar_url = $avatar_url->getScheme().'://'.$avatar_url->getHost().$avatar_url->getPath();
-                }
-            ]);
-            $guzzle->get($mediaResponse->media[0]->avatar_image);
             $directoryResponse = $guzzle->resolveResponseBody($directoryResponse, 'json');
+            $mediaResponse = $guzzle->get($directoryResponse->people->profile_image);
+            $mediaResponse = $guzzle->resolveResponseBody($mediaResponse, 'json');
             auth()->user()->directory_data = $directoryResponse->people;
-            auth()->user()->avatar_image = $avatar_url;
+            auth()->user()->avatar_image = $mediaResponse->avatar_image;
             session()->put('user', auth()->user());
             return redirect()->route('profile.edit.info', auth()->user()->email_uri);
         }
