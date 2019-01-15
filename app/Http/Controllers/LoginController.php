97a6ac2b23e2815f2304343c5a0458b5fc120e27
luisjg <luis.guzman@metalab.csun.edu>
2019-01-15 13:18:04 -0800

    Include cache busting.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,13 +2,25 @@
 
 namespace Alpha\Http\Controllers;
 
+use Carbon\Carbon;
 use CSUNMetaLab\Guzzle\Factories\HandlerGuzzleFactory;
-use GuzzleHttp\TransferStats;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    /**
+     * @var
+     */
+    private $mTimestamp;
+
+    /**
+     * LoginController constructor.
+     */
+    public function __construct()
+    {
+        $this->mTimestamp = Carbon::now();
+    }
+
     /**
      * @param Request $request
      * @return \Illuminate\Http\RedirectResponse
@@ -23,16 +35,27 @@ class LoginController extends Controller
             $directoryResponse = $guzzle->resolveResponseBody($directoryResponse, 'json');
             if (!empty($directoryResponse->people)) {
                 auth()->user()->directory_data = $directoryResponse->people;
-                auth()->user()->avatar_image = $directoryResponse->people->profile_image;
+                auth()->user()->avatar_image = $directoryResponse->people->profile_image.
+                    '?version='.$this->getFormattedTime();
             }
             if (auth()->user()->affiliation === 'student') {
                 $emailUri = strtok(auth()->user()->email, '@');
-                auth()->user()->avatar_image = config('webservices.media_url').auth()->user()->affiliation.'/media/'.$emailUri.'/avatar?secret='.config('webservices.media_secret_key');
+                auth()->user()->avatar_image = config('webservices.media_url').
+                    auth()->user()->affiliation.'/media/'.$emailUri.'/avatar?version='.
+                    $this->getFormattedTime().'&?secret='.config('webservices.media_secret_key');
             }
             session()->put('user', auth()->user());
             return redirect()->route('profile.edit.info', auth()->user()->email_uri);
         }
         session()->flash('message', 'Your username or password is invalid.');
         return redirect()->route('home');
+    }
+
+    /**
+     * @return string
+     */
+    private function getFormattedTime()
+    {
+        return $this->mTimestamp->year.$this->mTimestamp->hour.$this->mTimestamp->minute.$this->mTimestamp->second;
     }
 }
